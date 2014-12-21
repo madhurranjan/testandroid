eacd09f5e61e3d176c34374d568d33e745420d33
Madhurranjan Mohaan <madhurranjan@apigee.com>
2014-12-21 14:36:21 +0530

    Radio button example with timezones

--- app/src/main/java/com/example/madhurranjan/contactmanager/MainActivity.java
+++ app/src/main/java/com/example/madhurranjan/contactmanager/MainActivity.java
@@ -8,7 +8,16 @@ import android.view.MenuItem;
 import android.content.Intent;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
 import java.util.logging.ConsoleHandler;
 
 
@@ -24,6 +33,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
 
@@ -51,7 +61,7 @@ public class MainActivity extends ActionBarActivity {
 
 
 
-    public void registerDetails(View view){
+    /*public void registerDetails(View view){
         Intent intent = new Intent(this, PersonalDetails.class);
         EditText name = (EditText) findViewById(R.id.personName);
         EditText address = (EditText) findViewById(R.id.address);
@@ -70,5 +80,36 @@ public class MainActivity extends ActionBarActivity {
         intent.putExtra(EMAIL,personEmail);
 
         startActivity(intent);
+    }*/
+
+    public void showTime(View view) {
+        RadioGroup rg = (RadioGroup) findViewById(R.id.time);
+        Log.d("Mainactivity", String.valueOf(rg.getCheckedRadioButtonId()));
+        String timeValue= "";
+        DateFormat df = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+        TimeZone timeZone = TimeZone.getTimeZone("Europe/London");
+        Calendar cal = Calendar.getInstance(timeZone);
+        
+        switch(rg.getCheckedRadioButtonId()) {
+            case R.id.timeInGMT:
+                df.setTimeZone(timeZone);
+                timeValue = df.format(cal.getTime());
+                Log.d("MainActivity", timeValue);
+                break;
+            case R.id.timeInIST:
+                df.setTimeZone(TimeZone.getTimeZone("Asia/Calcutta"));
+                timeValue = df.format(cal.getTime());
+                Log.d("Mainactivity","TimeinIST");
+                break;
+            case R.id.timeInEST:
+                df.setTimeZone(TimeZone.getTimeZone("America/New_York"));
+                timeValue = df.format(cal.getTime());
+                Log.d("Mainactivity","TimeinPST");
+                break;
+            default:
+                Log.d("Mainactivity","noop");
+        }
+        Toast.makeText(this, timeValue,Toast.LENGTH_LONG).show();
+        rg.clearCheck();
     }
 }
